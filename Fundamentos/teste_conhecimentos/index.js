f86036b0f43fffc8f1e0aaa39038cd1740b750fe
thiago <thiagoe99@hotmail.com>
2022-08-30 09:32:32 -0300

    projeto do teste fim da sessao

---
+++ Fundamentos/teste_conhecimentos/index.js
@@ -0,0 +1,24 @@
+const inquirer = require("inquirer");
+const chalk = require("chalk");
+
+inquirer
+  .prompt([
+    {
+      name: "nome",
+      message: "Qual seu nome?",
+    },
+    {
+      name: "idade",
+      message: "Qual sua idade?",
+    },
+  ])
+  .then((answers) => {
+    if(answers.nome == ''){
+        throw new Error(chalk.bgRed("Campo de nome vazio!"))
+    }
+    if (answers.idade == "") {
+      throw new Error(chalk.bgRed("Campo de idade vazio!"));
+    }
+    console.log(chalk.bgYellow.black(`O(A) ${answers.nome} tem ${answers.idade} anos`));
+  })
+  .catch((err) => console.log(chalk.bgRed(err)));
