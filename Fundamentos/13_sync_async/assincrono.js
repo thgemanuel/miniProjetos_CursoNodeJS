f2bdce0a15bd3b154144dcd07b3f0b20927a0683
thiago <thiagoe99@hotmail.com>
2022-08-30 08:48:17 -0300

    projeto syncrono

---
+++ Fundamentos/13_sync_async/assincrono.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+
+console.log("inicio");
+
+fs.writeFile("arquivo.txt", "oi", function (err) {
+  setTimeout(function () {
+    console.log("arquivo criado!");
+  }, 1000);
+});
+
+console.log("fim");
+
+// inicio
+// fim
+// arquivo criado!
