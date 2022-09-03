1f5602c230d094b6683272a33a0332427e3a3616
thiago <thiagoe99@hotmail.com>
2022-09-02 22:27:15 -0300

    funcao operacoes de usuario

---
+++ SistemaBancarioNodeJS/index.js
@@ -0,0 +1,47 @@
+// modulos internos
+const fs = require("fs");
+
+// modulos externos
+const inquirer = require("inquirer");
+const chalk = require("chalk");
+
+operacoes();
+
+// funcao com operacoes que o usuario pode fazer no sistema
+function operacoes() {
+  // o metodo prompt da a possibilidade de escolher opções
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "action",
+        message: "Olá\nO que você deseja fazer?",
+        choices: [
+          "Criar conta",
+          "Consultar Saldo",
+          "Depositar",
+          "Sacar",
+          "Sair",
+        ],
+      },
+    ])
+    .then((answer) => {
+      //no then ficam as soluççoes da escolha do ususario
+      
+      //nessa action fica salva a acao seleciona dentra do array de respostas do usuario
+      const action = answer["action"];
+
+      if (action === "Criar conta") {
+        createAccount();
+      } else if (action === "Depositar") {
+        deposit();
+      } else if (action === "Consultar Saldo") {
+        getAccountBalance();
+      } else if (action === "Sacar") {
+        withdraw();
+      } else if (action === "Sair") {
+        console.log(chalk.bgBlue.black("Obrigado por usar o Accounts!"));
+        process.exit();
+      }
+    });
+}
