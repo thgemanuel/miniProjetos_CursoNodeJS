55401ee5533f9904548d0a82d2a0bc45ca351932
thiago <thiagoe99@hotmail.com>
2022-09-06 21:20:47 -0300

    adicionando renderizacao de HTML

--- Express/index.js
+++ Express/index.js
@@ -4,13 +4,19 @@ const express = require("express");
 const app = express();
 
 // definindo a porta
-const port = 3000;
+const port = 2000;
+
+const path = require("path");
+
+// o __dirname é refente ao diretorio atual
+const basePath = path.join(__dirname, "templates");
 
 // criar rota
 // requisicao(req) vem do cliente e a resposta(res) é enviada de volta
 // esta funcao diz o que é feito quando o usuario acessa o "/"
 app.get("/", (req, res) => {
-  res.send("Olá");
+  // retornando o arquivo html para renderizacao
+  res.sendFile(`${basePath}/index.html`);
 });
 
 // funcao para executar algo durante execucao do sistema
