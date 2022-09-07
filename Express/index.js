645307feed6bf066fa20cfd62a2d5d1c40012ac7
thiago <thiagoe99@hotmail.com>
2022-09-06 21:45:28 -0300

    adicionando middleware

--- Express/index.js
+++ Express/index.js
@@ -7,9 +7,26 @@ const app = express();
 const port = 2000;
 
 const path = require("path");
+const { setUncaughtExceptionCaptureCallback } = require("process");
 
 // o __dirname é refente ao diretorio atual
 const basePath = path.join(__dirname, "templates");
+
+// criando middleware para autenticar usuario
+const checkAuth = function (req, res, next) {
+  req.authStatus = true;
+//   dessa forma é possivel definir limitacoes, direcionar para uma pasta... 
+  if (req.authStatus) {
+    console.log("Logado!");
+    // o next é usado para que a aplicacao va para a proxima etapa, no caso renderizar o html
+    next();
+  } else {
+    console.log("Deslogado!");
+  }
+};
+
+// adicionando middleware na execução
+app.use(checkAuth);
 
 // criar rota
 // requisicao(req) vem do cliente e a resposta(res) é enviada de volta
